f2c84cc8315a0c31e02ec802aa333a8868c3f302
Darren Oakley <daz.oakley@gmail.com>
2015-07-01 17:04:52 +0100

    Update travis conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ Bundler.setup(:default, :test)
 
 require 'macmillan/utils/rspec/rspec_defaults'
 require 'macmillan/utils/rspec/rack_test_helper'
+require 'macmillan/utils/test_helpers/codeclimate_helper'
 require 'macmillan/utils/test_helpers/simplecov_helper'
 
 require 'pry'
