072fa2a17238b5fd459327efa5eed76a3af24933
Darren Oakley <daz.oakley@gmail.com>
2015-09-29 09:26:23 +0100

    Remove the geminabox helpers
    We’ve switched to using Gemfury so this is no longer needed.

--- lib/macmillan/utils/bundler/gem_helper.rb
+++
@@ -1,9 +0,0 @@
-module Bundler
-  class GemHelper
-    def rubygem_push(path)
-      gem_server_url = 'http://gems.npgsrv.com'
-      sh("gem inabox '#{path}' --host #{gem_server_url}")
-      Bundler.ui.confirm "Pushed #{name} #{version} to #{gem_server_url}"
-    end
-  end
-end
